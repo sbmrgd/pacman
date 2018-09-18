122ea6deb4de054ac7639b941cd19cb9988343cc
sbmrgd <37780354+sbmrgd@users.noreply.github.com>
2018-09-18 22:56:10 +0200

    Clean up code + implementation of clearScreen function

--- Game.h
+++ Game.h
@@ -41,54 +41,67 @@ class Game
     LetterController letterControllerOdd{4};
     SimpleRenderer ghostRenderer;
     DirectionalRenderer pacmanRenderer;
-    /*LetterController letter_C_controller{-4};
-    LetterController letter_M_controller{+4};
-    LetterController letter_A2_controller{-4};
-    LetterController letter_N_controller{+4};*/
+    bool updateScreen = false;
 
-    //Entity letter_P {letters_pal,letters[0],Pokitto::Display::getWidth()/2-3*18,Pokitto::Display::getHeight()/2-8+2,0,0,400, letter_P_controller};
-    //Entity letter_A {letters_pal,letters[1],Pokitto::Display::getWidth()/2-2*18,Pokitto::Display::getHeight()/2-8-2,0,0,400, letter_A_controller};
-    //Entity letter_C {letters_pal,letters[2],Pokitto::Display::getWidth()/2-1*18,Pokitto::Display::getHeight()/2-8+2,0,0,400, letter_C_controller};
-    //Entity letter_M {letters_pal,letters[3],Pokitto::Display::getWidth()/2-0*18,Pokitto::Display::getHeight()/2-8-2,0,0,400, letter_M_controller};
-    //Entity letter_A2 {letters_pal,letters[1],Pokitto::Display::getWidth()/2+1*18,Pokitto::Display::getHeight()/2-8+2,0,0,400, letter_A2_controller};
-    //Entity letter_N {letters_pal,letters[4],Pokitto::Display::getWidth()/2+2*18,Pokitto::Display::getHeight()/2-8-2,0,0,400, letter_N_controller};
-    //Entity pacman {pacman_pal,pacman_sprite[1],20,20,1,0,20, keyboardcontroller};
     std::vector<Entity> entities;
     std::vector<EntityController*> controllers;
     std::vector<EntityRenderer*> renderers;
 
-    void showTitleScreen()
+    void updateState()
     {
-        //letterControllerEven.update();
-        //letterControllerOdd.update();
-        //for(std::size_t index = 0;index<entities.size();++index)
         for(auto & controller : controllers)
         {
             (*controller).update();
         }
+
         for(auto & entity : entities)
         {
                 entity.update();
         }
+
         for(auto & renderer : renderers)
         {
             (*renderer).update();
         }
-        /*ghostRenderer.update();
-        pacmanRenderer.update();*/
+    }
+
+    void renderState()
+    {
         for(auto & entity : entities)
         {
-                entity.render();
+            entity.render();
         }
+
         for(std::size_t index = 0;index<entities.size();++index)
         {
-                Pokitto::Display::setSpriteBitmap(index,entities[index].bitmap,entities[index].palette,entities[index].position.x,entities[index].position.y);
+            if (!entities[index].changedBitmap) Pokitto::Display::setSpritePos(index,entities[index].position.x,entities[index].position.y);
+            else Pokitto::Display::setSpriteBitmap(index,entities[index].bitmap,entities[index].palette,entities[index].position.x,entities[index].position.y);
+
         }
-        Pokitto::Display::setFont(fontKoubit);
-        Pokitto::Display::setCursor(Pokitto::Display::getWidth()/2-6*Pokitto::Display::fontWidth,Pokitto::Display::getHeight()-Pokitto::Display::fontHeight-10);
-        Pokitto::Display::println("Press C to start");
-        if (Pokitto::Buttons::pressed(BTN_C)) gamestate = GameState::GamePlay;
     }
+
+    void showTitleScreen()
+    {
+
+        updateState();
+        renderState();
+
+        Pokitto::Display::setCursor(Pokitto::Display::getWidth()/2-4*Pokitto::Display::fontWidth,Pokitto::Display::getHeight()-Pokitto::Display::fontHeight-10);
+        Pokitto::Display::println("Insert (C)oin");
+
+        if (updateScreen)
+        {
+            Pokitto::Display::update();
+            updateScreen = false;
+        }
+        if (Pokitto::Buttons::pressed(BTN_C))
+        {
+                gamestate = GameState::GamePlay;
+                this->clearScreen();
+        }
+    }
+
+
     void update()
     {
         switch(this->gamestate)
@@ -97,6 +110,7 @@ class Game
             this->showTitleScreen();
             break;
         case GameState::GamePlay:
+            this->playGame();
             break;
         }
     }
@@ -104,33 +118,89 @@ class Game
     public:
     void begin()
     {
+        //Initialize Pokitto
         Pokitto::Core::begin();
         Pokitto::Core::setFrameRate(255);
+        Pokitto::Display::persistence = 1;
+        Pokitto::Display::setFont(fontKoubit);
 
+        //Initialize Controllers
         controllers.push_back(&letterControllerEven);
         controllers.push_back(&letterControllerOdd);
         controllers.push_back(&keyboardController);
 
+        //Initialize Renderers
         renderers.push_back(&ghostRenderer);
         renderers.push_back(&pacmanRenderer);
 
+        //Initialize Entities
         entities.emplace_back(letters_pal,letters[0],(int16_t)(Pokitto::Display::getWidth()/2-3*18),(int16_t)(Pokitto::Display::getHeight()/2-8+2),0,0,400, letterControllerEven);
         entities.emplace_back(letters_pal,letters[1],(int16_t)(Pokitto::Display::getWidth()/2-2*18),(int16_t)(Pokitto::Display::getHeight()/2-8-2),0,0,400, letterControllerOdd);
         entities.emplace_back(letters_pal,letters[2],(int16_t)(Pokitto::Display::getWidth()/2-1*18),(int16_t)(Pokitto::Display::getHeight()/2-8+2),0,0,400, letterControllerEven);
         entities.emplace_back(letters_pal,letters[3],(int16_t)(Pokitto::Display::getWidth()/2-0*18),(int16_t)(Pokitto::Display::getHeight()/2-8-2),0,0,400, letterControllerOdd);
         entities.emplace_back(letters_pal,letters[1],(int16_t)(Pokitto::Display::getWidth()/2+1*18),(int16_t)(Pokitto::Display::getHeight()/2-8+2),0,0,400, letterControllerEven);
         entities.emplace_back(letters_pal,letters[4],(int16_t)(Pokitto::Display::getWidth()/2+2*18),(int16_t)(Pokitto::Display::getHeight()/2-8-2),0,0,400, letterControllerOdd);
-        entities.emplace_back(pacman_pal,pacman_sprite[0],20,20,1,0,20, keyboardController, pacmanRenderer);
         entities.emplace_back(ghost_blinky_pal,ghost[0],5,5,1,0,20, keyboardController, ghostRenderer);
         entities.emplace_back(ghost_inky_pal,ghost[0],21,5,1,0,20, keyboardController, ghostRenderer);
         entities.emplace_back(ghost_clyde_pal,ghost[0],37,5,1,0,20, keyboardController, ghostRenderer);
         entities.emplace_back(ghost_pinky_pal,ghost[0],53,5,1,0,20, keyboardController, ghostRenderer);
+        entities.emplace_back(pacman_pal,pacman_sprite[0],20,20,1,0,20, keyboardController, pacmanRenderer);
+
+        for(std::size_t index = 0;index<entities.size();++index)
+        {
+                Pokitto::Display::setSpriteBitmap(index,entities[index].bitmap,entities[index].palette,entities[index].position.x,entities[index].position.y);
+        }
+        updateScreen = true;
+    }
+    void clearScreen()
+    {
+        for(int16_t x=0;x<Pokitto::Display::getHeight()/2+1;x++){
+        Pokitto::Display::directRectangle(Pokitto::Display::getWidth()/2-x*1.25, Pokitto::Display::getHeight()/2-x, Pokitto::Display::getWidth()/2+x*1.25 , Pokitto::Display::getHeight()/2+x,0);
+        Pokitto::Core::wait(10);
+        }
+
+        for(std::size_t index = 0;index<entities.size();++index)
+        {
+            entities[index].makeInvisible();
+            //Pokitto::Display::setSpritePos(index,300,300);
+        }
+        renderState();
+
+        /*TO DO: this part should be moved to a new function beginGame or InitializeGame or something
+        for(std::size_t index = 6;index<10;++index)
+        {
+            entities[index].position=Vector2{Pokitto::Display::getWidth()/2+16*(index-8),Pokitto::Display::getHeight()/2-8};
+        }
+
+        entities[10].position=Vector2{0,Pokitto::Display::getHeight()-16};
+        */
+
+        Pokitto::Display::fillScreen(0);
+        updateScreen = true;
+
+
+    }
+
+    void playGame()
+    {
+
+        Pokitto::Display::setColor(1);
+        Pokitto::Display::setCursor(10,10);
+        Pokitto::Display::println("gameplay");
+        if (updateScreen)
+        {
+            Pokitto::Display::update();
+        updateScreen=false;
+        }
+        updateState();
+        renderState();
+
     }
     void run()
     {
         while (Pokitto::Core::isRunning())
         {
-            if (Pokitto::Core::update())
+            if (Pokitto::Core::update(true))
             {
                 this->update();
             }
--- entity.h
+++ entity.h
@@ -30,6 +30,7 @@ public:
     Vector2 movement; // movement vector
     uint32_t time1 = Pokitto::Core::getTime(); // start time
     uint32_t update_time; // everytime that a number of ms equal to update_time have passed, the movement vector is added to the position vector
+    bool changedBitmap = false;
     /*Entity(const uint16_t* palette, const uint8_t* bitmap,uint16_t x, uint16_t y,EntityController & controller)
 		: controller(&controller)
 	{
@@ -41,39 +42,57 @@ public:
 private:
 	EntityController * controller; //Controller of the entity
 	EntityRenderer  * renderer = nullptr; //Renderer of the entity
+	bool isVisible = true;
 
 public:
     Entity(const uint16_t* palette, const uint8_t* bitmap,int16_t p_x, int16_t p_y, int16_t m_x, int16_t m_y, uint32_t update_time, EntityController & controller)
     : palette{palette}, bitmap{bitmap}, bitmap0{bitmap}, position{p_x,p_y}, movement{m_x,m_y}, update_time{update_time}, controller(&controller)
     {
+
     }
     Entity(const uint16_t* palette, const uint8_t* bitmap,int16_t p_x, int16_t p_y, int16_t m_x, int16_t m_y, uint32_t update_time,
            EntityController & controller, EntityRenderer & renderer)
     : palette{palette}, bitmap{bitmap}, bitmap0{bitmap}, position{p_x,p_y}, movement{m_x,m_y}, update_time{update_time}, controller(&controller), renderer(&renderer)
     {
+
     }
 	EntityController * getController(void) const
 	{
 		return this->controller;
 	}
+	void makeInvisible()
+	{
+	    isVisible = false;
+	    this->position=Vector2{300,300};
+	}
+	void makeVisible(Vector2 newPosition)
+	{
+	    isVisible = true;
+	    this->position=newPosition;
+	}
 	void update(void)
 	{
-		if(this->controller != nullptr)
-			this->controller->update(*this);
-        if((Pokitto::Core::getTime()-time1)>update_time)
+	    if (isVisible)
         {
-            time1 = Pokitto::Core::getTime();
-            this->position+=movement;
+            if(this->controller != nullptr)
+                this->controller->update(*this);
+            if((Pokitto::Core::getTime()-time1)>update_time)
+            {
+                time1 = Pokitto::Core::getTime();
+                this->position+=movement;
 
+            }
+
+            if (this->position.x<0)
+                this->position.x=0;
+            if (this->position.x>Pokitto::Display::getWidth()-bitmap[0])
+                this->position.x=Pokitto::Display::getWidth()-bitmap[0];
+            if (this->position.y<0)
+                this->position.y=0;
+            if (this->position.y>Pokitto::Display::getHeight()-bitmap[1])
+                this->position.y=Pokitto::Display::getHeight()-bitmap[1];
         }
-        if (this->position.x<0)
-            this->position.x=0;
-        if (this->position.x>Pokitto::Display::getWidth()-bitmap[0])
-            this->position.x=Pokitto::Display::getWidth()-bitmap[0];
-        if (this->position.y<0)
-            this->position.y=0;
-        if (this->position.y>Pokitto::Display::getHeight()-bitmap[1])
-            this->position.y=Pokitto::Display::getHeight()-bitmap[1];
+
 
 	}
 	void render(void)
